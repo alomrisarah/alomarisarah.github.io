e94ff1ae118405ac112125ad1b2f60708f19b469
Sara Alomari <alomarisoso95@gmail.com>
2024-09-03 21:33:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import './src/style.css';
-
-import saveIcon from './assets/icons/clipart.jpj';
-import editIcon from './assets/icons/edit-246.png';
-
 const itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : []
 document.getElementById("add").addEventListener("click", (e) => {
  if (item.value.length == 0) {
@@ -30,4 +25,80 @@ function displayItems() {
  activateDeleteListeners()
  activateEditListeners()
  activateSaveListeners()
+}
+
+function activateDeleteListeners() {
+ let deleteBtn = document.querySelectorAll(".deleteBtn")
+ deleteBtn.forEach((dB, i) => {
+  dB.addEventListener("click", () => {
+   deleteItem(i)
+  })
+ })
+}
+function changeIcon() {
+ const icon = document.getElementById("icon")
+
+ if (icon.src.includes("edit-246.png")) {
+  icon.src = "clipart.jpg"
+  icon.id = "save"
+ }
+}
+function changeIcon1() {
+ const icon = document.getElementById("icon")
+
+ if (icon.src.includes("clipart.jpg")) {
+  icon.src = "edit-246.png"
+ }
+}
+
+function activateEditListeners() {
+ const editBtn = document.querySelectorAll(".editBtn")
+ const inputs = document.querySelectorAll(".input-controller   textarea")
+ editBtn.forEach((eB, i) => {
+  eB.addEventListener("click", () => {
+   inputs[i].disabled = false
+   activateSaveListeners()
+  })
+ })
+}
+
+function activateSaveListeners() {
+ const saveBtn = document.querySelectorAll("#save")
+ const inputs = document.querySelectorAll(".input-controller  textarea")
+ saveBtn.forEach((sB, i) => {
+  sB.addEventListener("click", () => {
+   updateItem(inputs[i].value, i)
+   inputs[i].disabled = true
+  })
+  changeIcon1()
+ })
+}
+
+var tasks = document.querySelectorAll(".item")
+for (var i = 0; i < tasks.length; i++) {
+ tasks[i].onclick = function () {
+  this.classList.toggle("completed")
+ }
+}
+
+function createItem(item) {
+ itemsArray.push(item.value)
+ localStorage.setItem("items", JSON.stringify(itemsArray))
+ location.reload()
+}
+
+function deleteItem(i) {
+ itemsArray.splice(i, 1)
+ localStorage.setItem("items", JSON.stringify(itemsArray))
+ location.reload()
+}
+
+function updateItem(text, i) {
+ itemsArray[i] = text
+ localStorage.setItem("items", JSON.stringify(itemsArray))
+ location.reload()
+}
+
+window.onload = function () {
+ displayItems()
 }
