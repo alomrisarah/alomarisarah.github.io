ea66f39fc6ae92dc304b3191614c8dc78f498e4c
Sara Alomari <alomarisoso95@gmail.com>
2024-09-03 21:33:44 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from "path"
+import HtmlWebpackPlugin from "html-webpack-plugin"
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js', // Ensure this is the correct entry point
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-    }),
+export default {
+ mode: "development",
+ entry: "./src/index.js",
+ output: {
+  filename: "[name].bundle.js",
+  path: path.resolve(__dirname, "dist"),
+ },
+ devtool: "source-map",
+ devServer: {
+  contentBase: path.join(__dirname, "dist"),
+  compress: true,
+  port: 9000,
+ },
+ plugins: [
+  new HtmlWebpackPlugin({
+   template: path.resolve(__dirname, "./src/index.html"),
+  }),
+ ],
+ module: {
+  rules: [
+   {
+    test: /\.css$/i,
+    use: ["style-loader", "css-loader"],
+   },
+   {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    type: "asset/resource",
+   },
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+ },
+ optimization: {
+  runtimeChunk: "single",
+ },
+}
